b65e4f306b3133cf294ca033ea553587e7406bc2
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-06-11 11:24:08 -0300

    feat: accordion panel

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-let accordion = document.getElementById('accord').addEventListener('click', ()=>{
-  console.log('cliquei pô');
-})
+let accordion = document.getElementsByClassName('accordion')
+let panel = document.getElementsByClassName('panelAccord');
+let icon = document.getElementsByClassName('icon');
+
+for(let i = 0; i < accordion.length; i++){
+  accordion[i].addEventListener('click', () => {
+    if(panel[i].style.display.includes('block')){
+      panel[i].style.display = 'none';
+      return icon[i].src='./assets/images/chevron-down.svg'
+    }
+    icon[i].src='./assets/images/chevron-up.svg'
+    return panel[i].style.display = "block";
+  })
+
+}
