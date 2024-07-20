5e991b94cb9abeacd5803b74035df1d1a241fd99
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-07-20 14:31:10 -0300

    refactor: footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export function Footer() {
+  return (
+    <footer className="bg-white font-alegreyaBold flex flex-1 py-10 px-8">
+      <p>
+        Coded by{" "}
+        <a href="https://github.com/gomessgbr" target="_blank">
+          Gabriel Gomes
+        </a>
+        .
+      </p>
+    </footer>
+  );
+}
