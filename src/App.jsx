212aaa8e9d5942321eb4c44c5bb0020cff4e787d
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-07-20 15:39:28 -0300

    refactor: to public

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+
+import { Header, Footer } from "./components";
 import { Home } from "./pages/home/home";
 
 function App(){
   return(
     <>
+      <Header/>
       <Home/>
+      <Footer/>
     </>
   )
 }
