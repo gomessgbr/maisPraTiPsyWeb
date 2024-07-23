91ea1c33b9b76b4fcf20f2c6085416209bb6af95
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-07-23 19:36:35 -0300

    wip;page

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import chevronUp from '../../public/images/chevron-up.svg'
-import chevronDown from '../../public/images/chevron-down.svg'
+import chevronUp from '/images/chevron-up.svg'
+import chevronDown from '/images/chevron-down.svg'
 
 export function Accordion({ title, description }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,9 +10,9 @@ export function Accordion({ title, description }) {
   };
   return (
     <>
-      <div className="items-center border-y border-primary ">
+      <div className="border-y border-primary max-w-5xl ">
         <button
-          className=" flex items-center w-full p-8 font-semibold text-left rounded-t-md transition-all ease-in-out focus:outline-none"
+          className=" flex items-center w-full p-4 font-semibold text-left rounded-t-md transition-all ease-in-out focus:outline-none"
           onClick={toggleAccordion}
         >
           <span className="text-white font-montserratBold">{title}</span>
@@ -22,8 +22,8 @@ export function Accordion({ title, description }) {
           />
         </button>
         <div
-          className={` p-2.5 overflow-hidden transition-max-height duration-300 ease-in-out ${
-            isOpen ? "max-h-screen" : "max-h-0"
+          className={`p-2.5 overflow-hidden transition-max-height duration-300 ease-in-out ${
+            isOpen ? "grid" : "hidden"
           }`}
         >
           <div className="p-4 text-sm leading-normal text-white font-montserrat">
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -43,7 +43,9 @@ export function Home() {
           </p>
         </div>
         <div className="bg-black grid place-content-center">
-          <span className="font-montserratBold text-4xl text-white">Tipos de abordagens</span>
+          <span className="font-montserratBold text-4xl text-white">
+            Tipos de abordagens
+          </span>
 
           {therapies.length > 0 &&
             therapies.map(({ name, description }, index) => (
@@ -51,134 +53,8 @@ export function Home() {
                 key={`${index}+${name}`}
                 title={name}
                 description={description}
-    
               />
             ))}
-
-          {/* <ul>
-            <li>
-              <button type="button" className="accordion" id="accord">
-                <span>Terapia Cognitivo-Comportamental (TCC)</span>
-                <img
-                  className="icon"
-                  src="./assets/images/chevron-down.svg"
-                  alt=""
-                />
-              </button>
-              <div className="panelAccord" id="panelAccord">
-                <p>
-                  Focada em identificar e mudar padrões de pensamento negativos
-                  e comportamentos disfuncionais.
-                </p>
-              </div>
-            </li>
-            <li>
-              <button type="button" className="accordion" id="accord">
-                <span>Psicanálise</span>
-                <img
-                  className="icon"
-                  src="./assets/images/chevron-down.svg"
-                  alt=""
-                />
-              </button>
-              <div className="panelAccord" id="panelAccord">
-                Explora os pensamentos e sentimentos inconscientes, muitas vezes
-                relacionando experiências passadas e sua influência no
-                comportamento atual.
-              </div>
-            </li>
-            <li>
-              <button type="button" className="accordion" id="accord">
-                <span>Terapia Humanista</span>
-                <img
-                  className="icon"
-                  src="./assets/images/chevron-down.svg"
-                  alt=""
-                />
-              </button>
-              <div className="panelAccord" id="panelAccord">
-                Centrada no desenvolvimento pessoal e na realização do potencial humano, enfatizando a empatia e a aceitação incondicional.
-              </div>
-            </li>
-            <li>
-              <button type="button" className="accordion" id="accord">
-                <span>Terapia Comportamental</span>
-                <img
-                  className="icon"
-                  src="./assets/images/chevron-down.svg"
-                  alt=""
-                />
-              </button>
-              <div className="panelAccord" id="panelAccord">
-                Foca na mudança de comportamentos específicos através de técnicas de condicionamento e reforço.
-              </div>
-            </li>
-            <li>
-              <button type="button" className="accordion" id="accord">
-                <span>Terapia Gestalt</span>
-                <img
-                  className="icon"
-                  src="./assets/images/chevron-down.svg"
-                  alt=""
-                />
-              </button>
-              <div className="panelAccord" id="panelAccord">
-                Enfatiza a consciência e a experiência presente, incentivando os clientes a perceber e entender seus sentimentos e comportamentos no momento.
-              </div>
-            </li>
-            <li>
-              <button type="button" className="accordion" id="accord">
-                <span>Terapia Sistêmica</span>
-                <img
-                  className="icon"
-                  src="./assets/images/chevron-down.svg"
-                  alt=""
-                />
-              </button>
-              <div className="panelAccord" id="panelAccord">
-                Examina as interações dentro de sistemas familiares e sociais, focando nas dinâmicas de relacionamento e comunicação.
-              </div>
-            </li>
-            <li>
-              <button type="button" className="accordion" id="accord">
-                <span>Terapia de Aceitação e Compromisso (ACT)</span>
-                <img
-                  className="icon"
-                  src="./assets/images/chevron-down.svg"
-                  alt=""
-                />
-              </button>
-              <div className="panelAccord" id="panelAccord">
-                Combina técnicas de mindfulness com estratégias de mudança de comportamento, ajudando os clientes a aceitar seus pensamentos e sentimentos enquanto se comprometem com ações alinhadas com seus valores.
-              </div>
-            </li>
-            <li>
-              <button type="button" className="accordion" id="accord">
-                <span>Terapia Dialética Comportamental (TDC)</span>
-                <img
-                  className="icon"
-                  src="./assets/images/chevron-down.svg"
-                  alt=""
-                />
-              </button>
-              <div className="panelAccord" id="panelAccord">
-                Uma forma de TCC que inclui técnicas de mindfulness e é usada principalmente para tratar transtornos de personalidade borderline.
-              </div>
-            </li>
-            <li>
-              <button className="accordion" id="accord">
-                <span>Terapia Narrativa</span>
-                <img
-                  className="icon"
-                  src="./assets/images/chevron-down.svg"
-                  alt=""
-                />
-              </button>
-              <div className="panelAccord" id="panelAccord">
-                Ajuda os clientes a reescrever suas histórias pessoais, focando em como as narrativas pessoais influenciam a identidade e o comportamento.
-              </div>
-            </li>
-          </ul> */}
         </div>
       </main>
     </>
