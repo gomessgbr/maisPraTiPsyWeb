161a15dc822a1be4b54af6a23d38487657deb65a
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-07-25 20:50:17 -0300

    refactor: white background footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="bg-white font-alegreyaBold flex flex-1 py-10 px-8">
+    <footer className="bg-[#FFF] font-alegreyaBold flex flex-1 py-10 px-8 border-t border-black">
       <p>
         Coded by{" "}
         <a href="https://github.com/gomessgbr" target="_blank" className="underline">
