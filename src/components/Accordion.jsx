125eb6bf70a62841014b53e6244824744753f556
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-07-25 18:52:20 -0300

    refactor: change width accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import chevronUp from '/images/chevron-up.svg'
-import chevronDown from '/images/chevron-down.svg'
+import chevronUp from "/images/chevron-up.svg";
+import chevronDown from "/images/chevron-down.svg";
 
 export function Accordion({ title, description }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,28 +9,28 @@ export function Accordion({ title, description }) {
     setIsOpen(!isOpen);
   };
   return (
-    <>
-      <div className="border-y border-primary max-w-5xl ">
-        <button
-          className=" flex items-center w-full p-4 font-semibold text-left rounded-t-md transition-all ease-in-out focus:outline-none"
-          onClick={toggleAccordion}
-        >
-          <span className="text-white font-montserratBold">{title}</span>
-          <img
-            className={`ml-auto text-xs transition-transform duration-300`}
-            src={isOpen ? chevronUp : chevronDown }
-          />
-        </button>
-        <div
-          className={`p-2.5 overflow-hidden transition-max-height duration-300 ease-in-out ${
-            isOpen ? "grid" : "hidden"
+    <div className="w-full max-w-full border-y border-primary  overflow-hidden">
+      <button
+        className="flex  items-center min-w-full p-4 font-semibold text-left rounded-t-md transition-all ease-in-out focus:outline-none"
+        onClick={toggleAccordion}
+      >
+        <span className="text-white font-montserratBold">{title}</span>
+        <img
+          className={`ml-auto text-xs transition-transform duration-300 ${
+            isOpen ? "rotate-180" : ""
           }`}
-        >
-          <div className="p-4 text-sm leading-normal text-white font-montserrat">
-            {description}
-          </div>
-        </div>
+          src={isOpen ? chevronUp : chevronDown}
+        />
+      </button>
+      <div
+        className={`  p-2.5 transition-all duration-300 ease-in-out  ${
+          isOpen ? "max-h-screen" : "max-h-0"
+        }`}
+      >
+        <p className="p-4 text-sm leading-normal text-white font-montserrat">
+          {description}
+        </p>
       </div>
-    </>
+    </div>
   );
 }
