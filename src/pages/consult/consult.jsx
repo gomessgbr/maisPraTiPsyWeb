285e3e402002d19a5c0da7907766b64cdfe77fe1
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-07-25 21:55:46 -0300

    wip: consult page

---
+++ src/pages/consult/consult.jsx
@@ -0,0 +1,25 @@
+import { ProfissionalCard } from "../../components";
+
+export function Consult() {
+  return (
+    <>
+      <div className="containerConsult">
+        <form id="searchForm" className="relative bg-primary p-32 flex  justify-center i">
+          <label className="flex flex-col items-center">
+            <p className="font-montserratBold text-center mb-4">Digite o nome do profissional:</p>
+            <input
+              type="search"
+              name=""
+              id="searchInput"
+              className=" min-w-72 px-2 py-3 rounded-md border-0 shadow-md shadow-black"
+            />
+          </label>
+          <button type="submit" className="bg-accent px-4 py-4 rounded-md font-montserrat hover:bg-black hover:text-primary" form="searchForm">
+            buscar
+          </button>
+        </form>
+        <ProfissionalCard/>
+      </div>
+    </>
+  );
+}
