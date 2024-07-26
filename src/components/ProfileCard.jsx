1e540aff26b6decd6053ddf329d644b72656b02c
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-07-26 20:00:28 -0300

    feat: profile card component

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -2,9 +2,9 @@ import anaJuliaImage from "../../public/images/ana-silva.jpg";
 
 export function ProfileCard({ profileImg, about, phone, crp, name, approach }) {
   return (
-    <div className="max-w-[1024px] m-auto">
+    <div className=" max-w-[1024px] m-auto">
       <div className=" grid shadow-[4px_4px_4px_4px_rgba(0,0,0,0.25)] rounded-md p-9 my-10 mx-10">
-        <div className="flex gap-8 ">
+        <div className="grid grid-cols-[auto_1fr] gap-8">
           <img
             className="rounded-full w-[180px] h-[180px] object-cover"
             src={anaJuliaImage}
@@ -12,30 +12,28 @@ export function ProfileCard({ profileImg, about, phone, crp, name, approach }) {
           />
 
           <div>
-            <div>
-              <span className="flex justify-between">
-                <p>Ana Silva</p>
-                <p>CRP:06/123456</p>
+            <div className="grid gap-2 mb-4">
+              <span className="flex justify-between ">
+                <p className="font-montserratBold text-2xl">{name}</p>
+                <p className="font-semibold">CRP:{crp}</p>
               </span>
-              <span>Abordagem: Terapia Cognitiva-Comportamental</span>
+              <span>
+                <strong>Abordagem:</strong> {approach}
+              </span>
             </div>
             <span>
               <strong>Sobre mim</strong>
-              <p>
-                Psicóloga com 10 anos de experiência em terapia
-                cognitivo-comportamental, especializada em tratamento de
-                ansiedade e depressão.
-              </p>
+              <p>{about}</p>
             </span>
-            <div>
+            <div className="flex flex-row-reverse justify-between place-items-center mt-2">
               <a
-                href="https://api.whatsapp.com/send?phone5511975950915&text=Olá! Gostaria de fazer terapia com você"
+                href={`https://api.whatsapp.com/send?phone${phone}5&text=Olá! Gostaria de fazer terapia com você`}
                 target="_blank"
+                className="bg-[#34AF23] text-[#FFF] font-semibold p-[1em] rounded-[16px]"
               >
-                {" "}
                 Quero me consultar
               </a>
-              <span className="block mt-2">Telefone: (11) 98765-4321</span>
+              <span className="block mt-2">Telefone: {phone}</span>
             </div>
           </div>
         </div>
