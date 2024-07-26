0064e7e08a628a6bc650c5f0139fbf357bed2fbb
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-07-26 17:56:24 -0300

    feat: search input

--- src/pages/consult/consult.jsx
+++ src/pages/consult/consult.jsx
@@ -1,25 +1,33 @@
 import { ProfileCard } from "../../components";
 
-
 export function Consult() {
   return (
     <>
       <div className="containerConsult">
-        <form id="searchForm" className="relative bg-primary p-32 flex  justify-center i">
-          <label className="flex flex-col items-center">
-            <p className="font-montserratBold text-center mb-4">Digite o nome do profissional:</p>
+        <form
+          id="searchForm"
+          className="max-h-fit bg-primary p-32 flex justify-center gap-2 "
+        >
+          <label className="max-w-4xl w-full flex-grow">
+            <p className="font-montserratBold text-center mb-4">
+              Digite o nome do profissional:
+            </p>
             <input
               type="search"
               name=""
               id="searchInput"
-              className=" min-w-72 px-2 py-3 rounded-md border-0 shadow-md shadow-black"
+              className="max-w-4xl w-full px-2 py-3 rounded-md border-0 shadow-md shadow-black"
             />
           </label>
-          <button type="submit" className="bg-accent px-4 py-4 rounded-md font-montserrat hover:bg-black hover:text-primary" form="searchForm">
+          <button
+            type="submit"
+            className="bg-accent h-12  mt-11 px-4 py-2 rounded-md font-montserrat  hover:bg-black hover:text-primary"
+            form="searchForm"
+          >
             buscar
           </button>
         </form>
-        <ProfileCard/>
+        <ProfileCard />
       </div>
     </>
   );
