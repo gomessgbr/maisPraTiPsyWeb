f507cdc3768a29b23dffb869d97317b33cfe2d3e
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-07-26 21:38:27 -0300

    feat:  mobile first pages

--- src/pages/consult/consult.jsx
+++ src/pages/consult/consult.jsx
@@ -11,9 +11,9 @@ export function Consult() {
       <div className="min-h-screen">
         <form
           id="searchForm"
-          className="max-h-fit bg-primary p-32 flex justify-center gap-2 "
+          className="max-h-fit bg-primary p-4 sm:p-8 md:p-16 lg:p-32 flex flex-col sm:flex-row justify-center gap-2"
         >
-          <label className="max-w-4xl w-full flex-grow">
+          <label className="w-full flex-grow mb-4 sm:mb-0 max-w-xl">
             <p className="font-montserratBold text-center mb-4">
               Digite o nome do profissional:
             </p>
@@ -21,31 +21,33 @@ export function Consult() {
               type="search"
               name=""
               id="searchInput"
-              className="max-w-4xl w-full px-2 py-3 rounded-md border-0 shadow-md shadow-black"
+              className="w-full px-2 py-3 rounded-md border-0 shadow-md shadow-black"
               onChange={(e) => setQuery(e.target.value)}
             />
           </label>
           <button
             type="submit"
-            className="bg-accent h-12  mt-11 px-4 py-2 rounded-md font-montserrat  hover:bg-black hover:text-primary"
+            className="bg-accent h-12 px-4 py-2 self-end rounded-md font-montserrat hover:bg-black hover:text-primary"
             form="searchForm"
           >
             buscar
           </button>
         </form>
 
-        {profissionals.length > 0 &&
-          profissionals.map((profissional) => (
-            <ProfileCard
-              key={profissional.CRP}
-              about={profissional.aboutMe}
-              approach={profissional.approach}
-              crp={profissional.CRP}
-              name={profissional.name}
-              phone={profissional.contact}
-              profileImg={profissional.profilePicture}
-            />
-          ))}
+        <div className="px-4 sm:px-8 md:px-16 lg:px-32">
+          {profissionals.length > 0 &&
+            profissionals.map((profissional) => (
+              <ProfileCard
+                key={profissional.CRP}
+                about={profissional.aboutMe}
+                approach={profissional.approach}
+                crp={profissional.CRP}
+                name={profissional.name}
+                phone={profissional.contact}
+                profileImg={profissional.profilePicture}
+              />
+            ))}
+        </div>
       </div>
     </>
   );
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,6 @@
 import { Accordion } from "../../components";
 import { useGetTherapyTypes } from "./hooks/useGetTherapyTypes";
-import panelImage from "/images/panelImage.jpg"
+import panelImage from "/images/panelImage.jpg";
 
 export function Home() {
   const { therapies } = useGetTherapyTypes();
@@ -11,25 +11,36 @@ export function Home() {
         <img
           src={panelImage}
           alt="amigos sorrindo em um fundo vazios"
-          className="object-cover w-full max-h-[500px]"
+          className="object-cover w-full max-h-[300px] sm:max-h-[400px] lg:max-h-[500px]"
         />
-        <div className="grid gap-4 absolute bottom-0 py-8 px-40">
-          <h1 className="text-6xl font-montserratBold text-white">Terapia Online!</h1>
-          <a href="./consult.html" className="bg-primary w-max p-2 rounded-md hover:bg-accent">Marcar uma conversa inicial inicial</a>
+        <div className="grid gap-4 absolute bottom-0 py-8 px-4 sm:px-20 md:px-32 lg:px-40">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-montserratBold text-white">
+            Terapia Online!
+          </h1>
+          <a
+            href="./consult.html"
+            className="bg-primary w-max p-2 rounded-md hover:bg-accent"
+          >
+            Marcar uma conversa inicial
+          </a>
         </div>
       </div>
       <main>
-        <div className="grid gap-4 px-80 py-20">
-          <span className="text-4xl font-montserratBold">Como funciona terapia online ?</span>
+        <div className="grid gap-4 px-4 sm:px-20 md:px-32 lg:px-40 py-10 sm:py-20">
+          <span className="text-2xl sm:text-3xl md:text-4xl font-montserratBold">
+            Como funciona terapia online?
+          </span>
           <p>
             A terapia online é uma modalidade de atendimento psicológico ou
-            psicoterapêutico realizada através de plataformas digitias, como
+            psicoterapêutico realizada através de plataformas digitais, como
             videochamadas, chats ou até mesmo trocas de e-mails.
           </p>
-          <span className="text-4xl font-montserratBold">Primeiros passos</span>
+          <span className="text-2xl sm:text-3xl md:text-4xl font-montserratBold">
+            Primeiros passos
+          </span>
           <p>
-            <strong>Escolha do terapeuta e abordagem:</strong>O primeiro passo é
-            escolher um terapeuta que ofereça serviços online. Muitos
+            <strong>Escolha do terapeuta e abordagem:</strong> O primeiro passo
+            é escolher um terapeuta que ofereça serviços online. Muitos
             profissionais de saúde mental listam essa opção em seus sites ou em
             plataformas especializadas. Existem várias abordagens terapêuticas
             disponíveis, como terapia cognitivo-comportamental, psicanálise,
@@ -37,14 +48,13 @@ export function Home() {
             e métodos.
           </p>
           <p>
-            <strong>Agendamento de Sessões:</strong>
-            As sessões são agendadas de forma similar à terapia presencial. O
-            cliente e o terapeuta combinam um horário que seja conveniente para
-            ambos.
+            <strong>Agendamento de Sessões:</strong> As sessões são agendadas de
+            forma similar à terapia presencial. O cliente e o terapeuta combinam
+            um horário que seja conveniente para ambos.
           </p>
         </div>
-        <div className="bg-black grid place-content-center py-20 px-80">
-          <span className="font-montserratBold text-4xl text-white pb-10">
+        <div className="bg-black grid place-content-center py-10 sm:py-20 px-4 sm:px-20 md:px-32 lg:px-40">
+          <span className="font-montserratBold text-2xl sm:text-3xl md:text-4xl text-white pb-5 sm:pb-10">
             Tipos de abordagens
           </span>
 
