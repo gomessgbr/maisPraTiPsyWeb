4623dd902b505762012afc4573e960e326bfab36
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-07-26 21:38:40 -0300

    feat: mobile first component

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,21 +1,20 @@
-
-
 export function ProfileCard({ profileImg, about, phone, crp, name, approach }) {
   return (
-    <div className=" max-w-[1024px] m-auto">
-      <div className=" grid shadow-[4px_4px_4px_4px_rgba(0,0,0,0.25)] rounded-md p-9 my-10 mx-10">
-        <div className="grid grid-cols-[auto_1fr] gap-8">
+    <div className="max-w-[1024px] m-auto my-4">
+      <div className="shadow-md rounded-md p-4 sm:p-8 grid gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-[auto_1fr] gap-4">
           <img
-            className="rounded-full w-[180px] h-[180px] object-cover"
+            className="rounded-full w-[120px] h-[120px] sm:w-[180px] sm:h-[180px] object-cover"
             src={profileImg}
             alt="foto de perfil do profissional psicológo"
           />
-
-          <div>
+          <div className="flex flex-col justify-between">
             <div className="grid gap-2 mb-4">
-              <span className="flex justify-between ">
-                <p className="font-montserratBold text-2xl">{name}</p>
-                <p className="font-semibold">CRP:{crp}</p>
+              <span className="flex justify-between">
+                <p className="font-montserratBold text-xl sm:text-2xl">
+                  {name}
+                </p>
+                <p className="font-semibold">CRP: {crp}</p>
               </span>
               <span>
                 <strong>Abordagem:</strong> {approach}
@@ -25,15 +24,16 @@ export function ProfileCard({ profileImg, about, phone, crp, name, approach }) {
               <strong>Sobre mim</strong>
               <p>{about}</p>
             </span>
-            <div className="flex flex-row-reverse justify-between place-items-center mt-2">
+            <div className="flex flex-col sm:flex-row-reverse justify-between place-items-center mt-2">
               <a
-                href={`https://api.whatsapp.com/send?phone${phone}5&text=Olá! Gostaria de fazer terapia com você`}
+                href={`https://api.whatsapp.com/send?phone=${phone}&text=Olá! Gostaria de fazer terapia com você`}
                 target="_blank"
-                className="bg-[#34AF23] text-[#FFF] font-semibold p-[1em] rounded-[16px]"
+                rel="noopener noreferrer"
+                className="bg-[#34AF23] text-[#FFF] font-semibold p-2 rounded-md mt-4 sm:mt-0"
               >
                 Quero me consultar
               </a>
-              <span className="block mt-2">Telefone: {phone}</span>
+              <span className="block mt-2 sm:mt-0">Telefone: {phone}</span>
             </div>
           </div>
         </div>
