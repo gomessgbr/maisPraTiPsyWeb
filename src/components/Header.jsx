26638b7be2167b0674488f3e53ee13e8957398c9
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-07-26 21:17:05 -0300

    feat: hamburguer menu for mobile screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,44 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
 
 export function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="flex flex-1 items-center justify-between py-7 px-12  bg-black text-primary">
-      <Link to='/' className="font-alegreyaBold text-5xl ">
+    <nav className="relative flex items-center justify-between py-7 px-12 bg-black text-primary">
+      <Link to="/" className="font-alegreyaBold text-5xl">
         PsyWeb
       </Link>
-      <ul className="flex gap-8 font-montserratBold">
+      <div className="lg:hidden">
+        <button
+          onClick={toggleMenu}
+          className="text-primary focus:outline-none"
+        >
+          {isOpen ? <X /> : <Menu />}
+        </button>
+      </div>
+      <ul
+        className={`${
+          isOpen ? "flex" : "hidden"
+        } absolute bg-black p-8 right-0 z-10 top-20 flex-col gap-8 font-montserratBold lg:flex lg:flex-row lg:static lg:bg-transparent lg:p-0 lg:gap-8`}
+      >
         <li>
-          <Link to='consult' className="hover:text-accent">
+          <Link to="/consult" className="hover:text-accent">
             Encontre um psicologo
           </Link>
         </li>
         <li>
-          <Link to="contact" className="hover:text-accent">
+          <Link to="/habits" className="hover:text-accent">
             Hábitos
           </Link>
         </li>
         <li>
-          <Link to="contact" className="hover:text-accent">
+          <Link to="/contact" className="hover:text-accent">
             Contato
           </Link>
         </li>
